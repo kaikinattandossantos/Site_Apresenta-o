a0c9f6a8f04d78633547504a4828f7c9d23f6d84
kaiki nattan dos santos guedes <kaikinattansg@gmail.com>
2025-03-21 10:53:32 -0300

    links

--- app.py
+++ app.py
@@ -7,5 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 def home():
   return render_template('index.html') 
+
+
+@app.route('/sobremim')
+def sobremim():
+   return render_template('sobremim.html')
+
+
+@app.route('/Competencias')
+def competencias():
+   return render_template('competencias.html')
+
+
+@app.route('/Projetos')
+def Projetos():
+   return render_template('Projetos.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
